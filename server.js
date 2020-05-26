8afc67d70b8eb5667f547d230b89ca857fd1c334
HuyN1349 <huy.nguyen1349@gmail.com>
2020-05-25 21:46:05 -0700

    First commit.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -37,12 +38,12 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
+//Routing for API
 apiRoutes(app);
 
 //Sample Front-end
 
-    
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
